b40947a8d545ead43d7f126eb23f9cf3cd51da4c
Vijayeswar-Yalamanchili <vijayeswar_y@yahoo.com>
2024-02-06 13:33:05 +0530

    updated

--- AUTH-BE/src/helper/EmailService.js
+++ AUTH-BE/src/helper/EmailService.js
@@ -7,9 +7,10 @@ const transporter = nodemailer.createTransport({
         user: 'vijayeswarybe@gmail.com',
         pass: 'fogaxljrtqwrblqr'        //foga xljr tqwr blqr
       },
-      tls: {
-        rejectUnauthorized: false
-      }
+
+      // tls: {
+      //   rejectUnauthorized: false
+      // }
 });
 
 let mail = async(to,randomString) => {
